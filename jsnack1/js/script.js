2efe60dea1ec167d30d6eac9eb0ed31b03058f39
DNParadox <m96francesco@gmail.com>
2022-06-09 15:45:04 +0200

    Esercizi completi

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,20 +1,28 @@
+// Crea due tag div con due id diversi:
+// un div avrà il testo colorato di rosso mentre l’altro di verde.
+// Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e
+// in verde i numeri pari.
+
+// Creo un array contenti dei numeri -> Funziona
 const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 console.log(myNumbers);
 
+// srotolo l'array per controllare i numeri
 for (let i = 0; i < myNumbers.length; i++) {
-    const controlloArray = myNumbers[i]
-
-    let evenorodd;
+    let controlloArray = myNumbers[i]
+ 
+    let evenOrOdd;
+    // Se i numeri sono pari o dispari andranno in uno dei due div
     if(controlloArray  % 2 === 0) {
-        console.log(controlloArray);
-        document.getElementById('green').innerHTML = controlloArray + " "
+        console.log('pari',controlloArray);
+        document.getElementById('green').innerHTML += controlloArray + " "
 
     } else if (controlloArray % 2 !== 0) {
-        console.log(controlloArray)
+        console.log('dispari',controlloArray)
+        document.getElementById('red').innerHTML += controlloArray + " "
     }
 
-
-    }
+}
 
      
     
--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -2,4 +2,18 @@
 // Chiedi per 6 volte all’utente di inserire un numero,
 // se è dispari inseriscilo nell’array.
 
+// l'array è vuoto
 const odd = [];
+
+
+// Chiediamo con ciclo for di inserire un numero -> 
+for( let i = 0; i < 6; i++) {
+    const userNumbers = parseInt( prompt ('Dimmi un numero'))
+    console.log(userNumbers)
+
+    // Se il numero è dispari, inseriamo il contenuto nell'array
+    if (userNumbers % 2 !== 0) {
+        odd.push(userNumbers)
+        console.log(odd)
+    } 
+}
